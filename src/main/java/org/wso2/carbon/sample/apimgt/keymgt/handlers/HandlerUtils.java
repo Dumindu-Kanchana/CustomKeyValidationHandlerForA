3350221832f2ab5ca4964aa6a5ccf679df7a6538
Dumindu-Kanchana <dkgeeganage@gmail.com>
2019-03-27 09:33:13 +0530

    catch file not found exception

--- src/main/java/org/wso2/carbon/sample/apimgt/keymgt/handlers/HandlerUtils.java
+++ src/main/java/org/wso2/carbon/sample/apimgt/keymgt/handlers/HandlerUtils.java
@@ -1,13 +1,17 @@
 package org.wso2.carbon.sample.apimgt.keymgt.handlers;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
 public class HandlerUtils {
-
+    private static Log log = LogFactory.getLog(HandlerUtils.class);
     public static List<String> consumerKeyList = new LinkedList<>();
 
     public static void readSMSOtpConfig(String filePath) {
@@ -17,6 +21,8 @@ public class HandlerUtils {
             while ((sCurrentLine = br.readLine()) != null) {
                 consumerKeyList.add(sCurrentLine.trim());
             }
+        } catch (FileNotFoundException e) {
+            log.error("Flie cannot be found to Whitelist clientId's from scope validation");
         } catch (IOException e) {
             e.printStackTrace();
         }
